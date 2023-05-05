28171f847aa0bd9ce32139d1084075fa73d56cd3
Prathamesh Patil <prathameshpatil2812@gmail.com>
2023-05-05 19:31:42 +0000

    first program

--- First Program/main.cpp
+++ First Program/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-  std::cout << "Hello World!\n";
+
+  int num = 'a';
+  cout << num << endl;
+
+  char chr = 97;
+  cout << chr << endl;
+
+  int num1 = 2.0/5;
+  float num2 = 2.0/5;
+  double num3 = 2.0/5;
+  cout << num1 << " " << num2 << " " << num3 << endl;
+  
 }
